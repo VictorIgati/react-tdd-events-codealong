9548f08bd6afa0f454774c4238e783e1737a61f4
VictorIgati <victor.igati@student.moringaschool.com>
2025-01-24 15:22:58 +0300

    done everything

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React, { useState } from 'react';
+
 function App() {
+  const [pepperoniIsChecked, setPepperoniIsChecked] = useState(false);
+
+  function togglePepperoni(e) {
+    setPepperoniIsChecked(e.target.checked);
+  }
+
   return (
-    <div></div>
-  )
+    <div className="container">
+      <h1>Select Pizza Toppings</h1>
+      <div className="checkbox-container">
+        <input
+          type="checkbox"
+          id="pepperoni"
+          checked={pepperoniIsChecked}
+          aria-checked={pepperoniIsChecked}
+          onChange={togglePepperoni}
+        />
+        <label htmlFor="pepperoni">Add pepperoni</label>
+      </div>
+
+      <h2>Your Toppings:</h2>
+      <ul className="toppings-list">
+        <li>Cheese</li>
+        {pepperoniIsChecked && <li>Pepperoni</li>}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
